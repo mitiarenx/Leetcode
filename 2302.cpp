f61845aa29c331a97db95bab4a4dec547a321f1e
mitiarenx <mitandri@c1r13s9.42antananarivo.mg>
2025-04-28 15:36:40 +0300

    2302

---
+++ 2302.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+long long	countSubarrays( std::vector<int> &nums, long long k )
+{
+	long long	result = 0;
+	int			lastOk = -1, index = -1;
+
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		if (nums[i] )
+	}
+}
+
+int	main()
+{
+	std::vector<int>	nums = { 2, 1, 3, 4, 5 };
+
+	std::cout << std::endl << "=================== RESULT ===================" << std::endl;
+	std::cout << "SUBARRAYS = " << countSubarrays(nums, 10) << std::endl;
+	return 0;
+}
