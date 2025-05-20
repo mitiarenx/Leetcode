f2a5447a0d041f2db9e5fc1f16a2dd62d6d3b973
mitiarenx <mitiasoafinaritra@gmail.com>
2025-05-20 10:57:31 +0300

    3355

---
+++ 3355.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+bool	isZeroArray( std::vector<int> &nums, std::vector<std::vector<int>> &queries )
+{
+
+}
+
+int	main()
+{
+	std::vector<int>				nums = { 4, 3, 2, 1 };
+	std::vector<std::vector<int>>	queries = { { 1, 3 }, { 0, 2 } };
+	
+	std::cout << std::endl << "=================== RESULT ===================" << std::endl;
+
+	return 0;
+}
